62e5086f169bc98c279eb49edf18ae7491ac520c
João Marcos <joao.araujo@devsquad.com>
2023-03-04 08:53:09 -0300

    creating email factory

---
+++ app/Models/Email.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Str;
+
+class Email extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'to',
+        'body',
+    ];
+
+    protected function body(): Attribute
+    {
+        return Attribute::get( fn (string $value) => Str::limit($value, 20));
+    }
+
+    public function person(): HasOne
+    {
+        return $this->hasOne(Person::class, 'email', 'to');
+    }
+}
---
+++ database/factories/EmailFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Email;
+use App\Models\Person;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class EmailFactory extends Factory
+{
+    protected $model = Email::class;
+
+    public function definition(): array
+    {
+        return [
+            'to' => Person::factory()->create()->email,
+            'body' => $this->faker->realText
+        ];
+    }
+}
